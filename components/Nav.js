d724d2de4399a0e3be888bca195f637e31eaaf37
Luis <luiscoimbraeng@outlook.com>
2022-06-09 23:29:45 -0300

    [Bug] Y scroll hided

--- components/Nav.js
+++ components/Nav.js
@@ -5,11 +5,12 @@ function Nav() {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-scroll scrollbar-hide ">
+      {/* Could improve here to scroll right when try to scroll down here */}
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 scrollbar-hide overflow-y-hidden overflow-x-scroll">
         {Object.entries(requests).map(([key, { title }]) => (
           <h2
             key={key}
-            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 "
+            className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 "
             onClick={() => router.push(`/?genre=${key}`)}
           >
             {title}
